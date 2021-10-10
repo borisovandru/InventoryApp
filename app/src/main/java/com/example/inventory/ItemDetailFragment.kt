d76c725e8e95422c1c8297e504e345e9e07a47a5
konstantin <kosval09@gmail.com>
2021-10-10 13:06:11 +0900

    кнопка delete

--- app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -76,6 +76,7 @@ class ItemDetailFragment : Fragment() {
             //отключает кнопку cell
             sellItem.isEnabled = viewModel.isStockAvailable(item)
             sellItem.setOnClickListener { viewModel.sellItem(item) }
+            deleteItem.setOnClickListener { showConfirmationDialog() }
         }
     }
     /**
@@ -97,6 +98,7 @@ class ItemDetailFragment : Fragment() {
      * Deletes the current item and navigates to the list fragment.
      */
     private fun deleteItem() {
+        viewModel.deleteItem(item)
         findNavController().navigateUp()
     }
 
